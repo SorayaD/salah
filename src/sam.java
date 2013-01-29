3a5dbab331ab8b8044e30c57c7bb1208b9c4a963
soraya kerroum <soraya.kerroum.algerie@viaseci.fr>
2013-01-29 12:54:47 +0100

    ez

---
+++ src/sam.java
@@ -0,0 +1,50 @@
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JButton;
+import java.awt.BorderLayout;
+import javax.swing.JRadioButton;
+import java.awt.GridLayout;
+
+
+public class sam {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					sam window = new sam();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public sam() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(new GridLayout(1, 0, 0, 0));
+		
+		JButton btnNewButton = new JButton("New button");
+		frame.getContentPane().add(btnNewButton);
+	}
+
+}
